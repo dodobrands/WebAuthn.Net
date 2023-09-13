aa042be5b0160f35815fa513f68e4097752ade3f
Roman Bukin <r.bukin@dodopizza.com>
2023-09-13 21:48:11 +0300

    Fix link to specification.

--- src/WebAuthn.Net/Models/Protocol/Request/AuthenticationExtensionsClientInputs.cs
+++ src/WebAuthn.Net/Models/Protocol/Request/AuthenticationExtensionsClientInputs.cs
@@ -4,7 +4,7 @@
 ///     Additional parameters requesting additional processing by the client and authenticator.
 /// </summary>
 /// <remarks>
-///     <a href="https://www.w3.org/TR/webauthn/#iface-authentication-extensions-client-inputs">Web Authentication: An API for accessing Public Key Credentials Level 2 - § 5.7.1. Authentication Extensions Client Inputs</a>
+///     <a href="https://www.w3.org/TR/webauthn-2/#iface-authentication-extensions-client-inputs">Web Authentication: An API for accessing Public Key Credentials Level 2 - § 5.7.1. Authentication Extensions Client Inputs</a>
 /// </remarks>
 public class AuthenticationExtensionsClientInputs
 {
