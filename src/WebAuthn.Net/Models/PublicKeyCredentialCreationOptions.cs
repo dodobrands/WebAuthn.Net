a104428df8c5c56ffbf4257698eab57b488dbb19
George Polevoy <george.polevoy@gmail.com>
2023-09-13 18:09:19 +0300

    credentialcreationoptions WIP

--- src/WebAuthn.Net/Models/PublicKeyCredentialCreationOptions.cs
+++ src/WebAuthn.Net/Models/PublicKeyCredentialCreationOptions.cs
@@ -1,3 +1,9 @@
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
 namespace WebAuthn.Net.Models;
 
 /// <summary>
@@ -5,15 +11,39 @@ namespace WebAuthn.Net.Models;
 /// </summary>
 public class PublicKeyCredentialCreationOptions
 {
-    // required PublicKeyCredentialRpEntity         rp;
-    // required PublicKeyCredentialUserEntity       user;
-    //
-    // required BufferSource                             challenge;
-    // required sequence<PublicKeyCredentialParameters>  pubKeyCredParams;
-    //
-    // unsigned long                                timeout;
-    // sequence<PublicKeyCredentialDescriptor>      excludeCredentials = [];
-    // AuthenticatorSelectionCriteria               authenticatorSelection;
-    // DOMString                                    attestation = "none";
-    // AuthenticationExtensionsClientInputs         extensions;
+    [Required]
+    [JsonPropertyName("rp")]
+    public PublicKeyCredentialRpEntity Rp { get; }
+
+    [Required]
+    [JsonPropertyName("user")]
+    public PublicKeyCredentialUserEntity User { get; }
+
+    [Required]
+    [JsonPropertyName("challenge")]
+    public byte[] Challenge { get; }
+
+    [Required]
+    [JsonPropertyName("pubKeyCredParams")]
+    public ReadOnlyCollection<PublicKeyCredentialParameters> PubKeyCredParams { get; }
+
+    [JsonPropertyName("timeout")]
+    public ulong Timeout { get; }
+
+    [JsonPropertyName("excludeCredentials")]
+    public ReadOnlyCollection<PublicKeyCredentialDescriptor> ExcludeCredentials { get; }
+
+    [JsonPropertyName("authenticatorSelection")]
+    public AuthenticatorSelectionCriteria AuthenticatorSelection { get; }
+
+    [JsonPropertyName("attestation")]
+    public string Attestation { get; } = "none";
+
+    [JsonPropertyName("extensions")]
+    public AuthenticationExtensionsClientInputs Extensions { get; }
+}
+
+public class PublicKeyCredentialRpEntity
+{
+    // TODO: implement
 }
