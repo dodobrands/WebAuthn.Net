181080ca3f2734fb273ba62255911d3e89b1c3e9
Roman Bukin <r.bukin@dodopizza.com>
2023-09-15 14:11:30 +0300

    Refactoring

--- src/WebAuthn.Net/Services/Assertion/Implementation/AssertionService.cs
+++ src/WebAuthn.Net/Services/Assertion/Implementation/AssertionService.cs
@@ -4,26 +4,26 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using WebAuthn.Net.Models;
 using WebAuthn.Net.Models.Abstractions;
-using WebAuthn.Net.Models.Protocol.Attestation;
-using WebAuthn.Net.Services.Attestation.Models;
+using WebAuthn.Net.Models.Protocol.Assertion;
+using WebAuthn.Net.Services.Assertion.Models;
 using WebAuthn.Net.Services.Context;
 
-namespace WebAuthn.Net.Services.Attestation.Implementations;
+namespace WebAuthn.Net.Services.Assertion.Implementation;
 
-public class AttestationService<TContext> : IAttestationService
+public class AssertionService<TContext> : IAssertionService
     where TContext : class, IWebAuthnContext
 {
     private readonly IWebAuthnContextFactory<TContext> _contextFactory;
 
-    public AttestationService(IWebAuthnContextFactory<TContext> contextFactory)
+    public AssertionService(IWebAuthnContextFactory<TContext> contextFactory)
     {
         ArgumentNullException.ThrowIfNull(contextFactory);
         _contextFactory = contextFactory;
     }
 
-    public async Task<CredentialCreationOptions> CreateOptionsAsync(
+    public async Task<CredentialRequestOptions> CreateOptionsAsync(
         HttpContext httpContext,
-        CredentialCreationOptionsRequest request,
+        CredentialRequestOptionsRequest request,
         CancellationToken cancellationToken)
     {
         cancellationToken.ThrowIfCancellationRequested();
@@ -31,9 +31,9 @@ public class AttestationService<TContext> : IAttestationService
         throw new NotImplementedException();
     }
 
-    public async Task<Result<HandleAttestationResponse>> HandleAsync(
+    public async Task<Result<HandleAssertionResponse>> HandleAsync(
         HttpContext httpContext,
-        HandleAttestationRequest request,
+        HandleAssertionRequest request,
         CancellationToken cancellationToken)
     {
         cancellationToken.ThrowIfCancellationRequested();
