46938d190ad1429d9fe0f1d71f9c91fa54fc5443
Roman Bukin <r.bukin@dodopizza.com>
2023-12-28 18:52:24 +0300

    Add reset endpoint.

--- demo/WebAuthn.Net.Demo.Mvc/Controllers/AccountController.cs
+++ demo/WebAuthn.Net.Demo.Mvc/Controllers/AccountController.cs
@@ -27,4 +27,19 @@ public class AccountController : Controller
 
         return RedirectToAction("Index", "Home");
     }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Reset()
+    {
+        foreach (var (key, _) in HttpContext.Request.Cookies)
+        {
+            if (!key.Contains("antiforgery", StringComparison.InvariantCultureIgnoreCase))
+            {
+                HttpContext.Response.Cookies.Delete(key);
+            }
+        }
+
+        return Ok();
+    }
 }
